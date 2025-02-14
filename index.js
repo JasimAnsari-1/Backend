0978a157b51235628ce3d0f07222d9caf7198000
Jasim Ansari <jasimcec2025@gmail.com>
2025-02-14 15:50:14 +0530

    Backend

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 const http = require("http");
-const fs = require("fs");
-const url =  require("url");
+const express = require("express");
 
 
+const app = express();
 
-const myServer = http.createServer((req,res) => {
-    if (req.url === "/favicon.ico")return res.end();
-    const log = `${Date.now()}: ${req.url} New Req Recieved\n`;
-    const myUrl = url.parse(req.url);
-    console.log(myUrl);
-    fs.appendFile("log.txt",log,(err,data) =>{
-        switch(req.url){
-            case "/": res.end("HomePage");
-            break
-            case "/about":res.end("I am Jasim");
-            break;
-            default:
-                res.end("404 Not Found");
-        }
-    })
+app.get("/",(req,res)=>{
+    return res.send("Hello");
 });
+app.get("/about",(req,res)=>{
+    return res.send("hello about page")
+});
+
+
+const myServer = http.createServer(app);
 myServer.listen(8000, ()=> console.log("Server Started"));
 
 
